d776280808077bbcd94ba7e06b3574451d8a6097
John Hotterbeekx <john.online@hotterbeekx.nl>
2019-01-26 23:22:07 +0100

    Switching icon on toggle

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.browserAction.onClicked.addListener(() => {
   chrome.storage.local.get(['catify_disabled'], (storage) => {
     chrome.storage.local.set({'catify_disabled': !storage.catify_disabled}, () => {
+      chrome.browserAction.setIcon({path: !storage.catify_disabled ? 'icons/16.disabled.png' : 'icons/16.png'});
       chrome.tabs.query({}, tabs => {
         tabs.forEach(tab => {
           chrome.tabs.reload(tab.id);
@@ -8,4 +9,8 @@ chrome.browserAction.onClicked.addListener(() => {
       });
     });
   });
+});
+
+chrome.storage.local.get(['catify_disabled'], (storage) => {
+  chrome.browserAction.setIcon({path: storage.catify_disabled ? 'icons/16.disabled.png' : 'icons/16.png'});
 });
